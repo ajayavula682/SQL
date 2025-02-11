4b7e681c133d89c49236782c3fb5d2cdf2217e00
avulaajaykumarreddy <ajayavula682@gmail.com>
2025-02-11 15:26:10 -0500

    added few new querries

--- basics.sql
+++ basics.sql
@@ -59,6 +59,79 @@ select name,marks from student
 where marks > 80 
 limit  2;
 
+# shows only two tuples in the given condition
+
+select city,name,count(rollno)
+from student
+group by city;
+
+select city,avg(marks)
+from student
+group by city
+order by city;
+
+select city,avg(marks)
+from student
+order by avg(marks);
+
+select grade,count(rollno)
+from student 
+group by grade order by grade;
+ # having clause used for conditions and the having that alter
+ # mostly applies on the rows.
+ # mainly it used for the when we want to apply after grouping.
+ 
+ select count(name),city
+ from student 
+ group by city
+ having max(marks) >90;
+ 
+start transaction;
+ 
+update student 
+set grade ="O"
+where grade="A";
+
+
+
+
+select * from student;
+
+show tables;
+
+show databases;
+
+use collage;
+
+
+create table payments (
+customer_id int primary key,
+customer char(20),
+mode char(20),
+city char(20));
+
+insert into payments 
+(customer_id,customer,mode,city)
+values
+(103,"Maya Hemandaz","credit card","seattle"),
+(104,"Liam","Netbanking","denver"),
+(105,"Sophia","creditcard","new orleans"),
+(106,"caleb","debit card","minneapolis"),
+(107,"Ava patel","debit card","pheonix"),
+(108,"lucas carter","netbanking","bostan"),
+(109,"isabella martinez","netbanking","nashivelle"),
+(110,"jackson brooks","credit card","cleveland");
+
+select * from payments;
+
+select mode,count(customer)
+from payments
+group by mode;
+
+
+
+
+
 
 
 
